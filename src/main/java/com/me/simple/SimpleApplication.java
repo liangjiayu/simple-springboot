5998d344f50a7ce7ee097c93e38796d3d2f82c49
LiangJiaYu <1013834609@qq.com>
2025-02-23 11:34:56 +0800

    fix

--- src/main/java/com/me/simple/SimpleApplication.java
+++ src/main/java/com/me/simple/SimpleApplication.java
@@ -11,7 +11,7 @@ public class SimpleApplication {
 
 	@RequestMapping("/list")
 	public String home() {
-		return "Hello World 123456";
+		return "Hello World 123456789";
 	}
 
 	public static void main(String[] args) {
